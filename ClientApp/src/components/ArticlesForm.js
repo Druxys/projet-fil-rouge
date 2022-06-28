4c86ad41c3e151881f84706c8d795925d7b48eb1
AhmedBouk <totoriina76380@gmail.com>
2022-06-28 18:01:30 +0200

    article form presque fini

--- ClientApp/src/components/ArticlesForm.js
+++ ClientApp/src/components/ArticlesForm.js
@@ -2,11 +2,11 @@
 import { useForm } from "react-hook-form";
 import {
     Box, Button, Grid, TextField, Select,
-    InputLabel, OutlinedInput, Fab, FormControl,
-    Typography, Autocomplete, MenuItem
+    InputLabel,  Fab, FormControl,
+    Typography,  MenuItem
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-
+const axios = require('axios');
 export const ArticlesForm = (props) => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const [articles, setArticles] = useState();
@@ -14,9 +14,11 @@ export const ArticlesForm = (props) => {
     const [operations, setOperations] = useState();
     const [dataList, setDataList] = useState([{ codeArticle: '', quantite: '' }]);
 
-    //console.log(dataList);
+    const [operationValue, setOperationValue] = useState()
+    const [categoryValue, setCategoryValue] = useState()
+
     //console.log(watch("example")); // watch input value by passing the name of it
-    const onSubmit = data => console.log(data);
+ 
 
     const getArticlesData = async () => {
         const response = await fetch('articles');
@@ -35,21 +37,21 @@ export const ArticlesForm = (props) => {
         const resultData = JSON.parse(data)
         setCategories(resultData);
     }
-
+    //console.log(dataList)
     useEffect(() => {
         getCategoriesData();
     }, []);
 
-    //const getOperationsData = async () => {
-    //    const response = await fetch('operations');
-    //    const data = await response.json();
-    //    const resultData = JSON.parse(data)
-    //    setOperations(resultData);
-    //}
+    const getOperationsData = async () => {
+        const response = await fetch('operations');
+        const data = await response.json();
+        const resultData = JSON.parse(data)
+        setOperations(resultData);
+    }
 
-    //useEffect(() => {
-    //    getOperationsData();
-    //}, []);
+    useEffect(() => {
+        getOperationsData();
+    }, []);
 
     const articleOptions = articles?.map((article) => ({
         label: article.code,
@@ -62,20 +64,16 @@ export const ArticlesForm = (props) => {
         value: category.code,
     }));
 
-    //const operationOptions = categories?.map((operation) => ({
-    //    label: operation.libelle,
-    //    value: operation.code,
-    //}));
+    const operationOptions = operations?.map((operation) => ({
+        label: operation.libelle,
+        value: operation.code,
+    }));
 
-    const isEmpty = !!categories;
-    console.log(isEmpty)
-    console.log(categories);
+   
+
 
     const handleChange = (e, index) => {
-
         const { name, value } = e.target;
-        console.log(e, index);
-        console.log(e.target);
         const list = [...dataList];
         list[index][name] = value;
         setDataList(list)
@@ -87,8 +85,17 @@ export const ArticlesForm = (props) => {
         list[index][name] = value;
         setDataList(list)
     };
+
+    const handleChangeOperation = (e) => {
+        setOperationValue(e.target.value)
+    };
+    
+    const handleChangeCategory = (e) => {
+        setCategoryValue(e.target.value)
+    };
+
     const handleDatalistAdd = () => {
-        setDataList([...dataList, { codeArticle: '', quantite: '' }])
+        setDataList([...dataList, {  codeArticle: '', quantite: '' }])
     }
 
     const handleDatalistRemove = (index) => {
@@ -96,7 +103,26 @@ export const ArticlesForm = (props) => {
         list.splice(index, 1)
         setDataList(list)
     }
-    console.log(dataList)
+
+
+    const onSubmit = (data) => { 
+        const articles = {articles: dataList}
+        const { codeCategorie, codeArticleNew, codeOperation } = data
+        const codeArticle = codeArticleNew
+        const values = { ...articles, codeCategorie, codeArticle,  codeOperation }
+      
+        const articleJson = JSON.stringify(values);
+        console.log(articleJson);
+
+        axios.post('articles', JSON.stringify(articleJson))
+            .then(response => {
+                console.log("Status: ", response.status);
+                console.log("Data: ", response.data);
+            }).catch(error => {
+                console.error('Something went wrong!', error);
+            });
+    };
+
     const ref0 = useRef();
     return (
         <Box sx={{ flexGrow: 1 }} mt="10rem">
@@ -108,10 +134,10 @@ export const ArticlesForm = (props) => {
                         </Typography>
                     </Grid>
                     <Grid item xs={6}>
-                        <TextField fullWidth defaultValue="test" {...register("example")} label="Nom de larticle" />
+                        <TextField fullWidth {...register("name")} label="Nom de larticle" />
                     </Grid>
                     <Grid item xs={6}>
-                        <TextField fullWidth  {...register("exampleRequired", { required: true })} label="Code article" />
+                        <TextField fullWidth  {...register("codeArticleNew", { required: true })} label="Code article" />
                         {errors.exampleRequired && <span>This field is required</span>}
                     </Grid>
                     <Grid item xs={12}>
@@ -120,15 +146,16 @@ export const ArticlesForm = (props) => {
 
                             <Select
                                 labelId="codeCategorie"
-                                value={categoryOptions}
-                                onChange={(e) => console.log(e)}
+                                value={categoryValue}
+                                onChange={handleChangeCategory}
                                 fullWidth
-                                name="codeArticle"
+                                {...register("codeCategorie")}
+                            
                             >
-                                {categoryOptions?.map((article) => {
+                                {categoryOptions?.map((category) => {
                                     return (
-                                        <MenuItem key={article?.value} value={article?.label}>
-                                            {article?.value}
+                                        <MenuItem key={category?.value} value={category?.label}>
+                                            {category?.value}
                                         </MenuItem>
                                     )
                                 }
@@ -144,6 +171,7 @@ export const ArticlesForm = (props) => {
                     </Grid>
 
                     {dataList.map((element, index) => (
+                    <>
                         <Grid container spacing={3} ml="5px" key={index}>
                             <Grid mb="2rem" item xs={5}>
                                 <FormControl fullWidth>
@@ -164,8 +192,9 @@ export const ArticlesForm = (props) => {
                                     <Select
                                         labelId="articleId"
                                         value={element?.codeArticle}
-                                        onChange={(e) => handleChange(e, index)}                                       
-                                        name="codeArticle"
+                                        onChange={(e) => handleChange(e, index)}
+                                        inputProps={register('codeArticle')}
+                                         
                                     >
                                         {articleOptions?.map((article) => {
                                             return (
@@ -184,7 +213,8 @@ export const ArticlesForm = (props) => {
                                     id="quantiteNumber"
                                     label="Number"
                                     type="number"
-                                    name="quantite"
+                                    value={element?.quantite}
+                                        {...register("quantite")}
                                     onChange={(e) => handleChangeQuantity(e, index)}
                                     InputLabelProps={{
                                         shrink: true,
@@ -211,22 +241,36 @@ export const ArticlesForm = (props) => {
                             )}
                         </Grid>
 
-
+</>
                     ))}
                     <Grid item xs={12}>
                         <FormControl fullWidth >
                             <InputLabel id="codeOperation">Opération</InputLabel>
+
                             <Select
-                                fullWidth
                                 labelId="codeOperation"
+                                value={operationValue}
+                                onChange={handleChangeOperation}
+                                fullWidth
                                 {...register("codeOperation")}
-                                input={<OutlinedInput label="Opération" />}
                             >
+                                {operationOptions?.map((operation) => {
+                                    return (
+                                        <MenuItem key={operation?.value} value={operation?.value}>
+                                            {operation?.label}
+                                        </MenuItem>
+                                    )
+                                }
+                                )
+                                }
                             </Select>
                         </FormControl>
                     </Grid>
+                    <Grid item xs={6}>
+                        <input type="submit" />
+                    </Grid>
                 </Grid>
-                <input type="submit" />
+              
             </form>
         </Box>
     );
